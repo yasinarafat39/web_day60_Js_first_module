d7b6dbe22a2b38baf07de8ec8cafab60f3443adc
Yasin Arafat <yasinarafat2557@gmail.com>
2024-08-29 19:58:17 +0600

    first commit

---
+++ practice-task.js
@@ -0,0 +1,26 @@
+// *********************** Task  1 **************************//
+
+const myMoney = 1000;
+const orangesPrice = 300;
+const applesPrice = 400;
+
+const totalCost = orangesPrice + applesPrice;
+const returnMoney = myMoney - totalCost;
+
+console.log(returnMoney);
+
+// *********************** Task  2 **************************//
+
+const Mathematics = 75.25;
+const Biology = 65;
+const Chemistry = 80;
+const Physics = 35.45;
+const Bangla = 99.5;
+
+const totalMark = Mathematics + Biology + Chemistry + Physics + Bangla;
+const averageMark = parseFloat((totalMark / 5).toFixed(2));
+
+console.log(averageMark);
+
+// *********************** Task  3 **************************//
+// *********************** Task  4 **************************//
